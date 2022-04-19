913b35636a18420f9b398393984d7ef8498e305d
Hans Huanca Aroni <hansaroni@hotmail.com>
2022-04-18 23:46:28 -0300

    cambios navbar

--- src/Button.js
+++ src/Button.js
@@ -3,9 +3,12 @@ import './Button.css'
 
 const STYLES = [
     'btn--primary',
+    'btn--outline'
+]
+const SIZES = [
+    'btn--medium',
     'btn--large'
 ]
-
 export const Button = ({
     children,
     type,
@@ -19,9 +22,7 @@ export const Button = ({
     const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
 
     return(
-        <button className={"btn ${checkButtonStyles} ${checkButtonSize}"} onClick={onClick} type={type}>
-            {children}
-        </button>
+        <button className={`btn ${checkButtonStyle} ${checkButtonSize}`} onClick={onClick} type={type}>{children}</button>
 
     )
 }
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './NavBar.css';
 import {MenuItems} from "./MenuItems"
+import { Button } from '../../Button';
 
 
 // function NavBar(props) {
@@ -18,7 +19,7 @@ import {MenuItems} from "./MenuItems"
 
 
 class NavBar extends Component{
-    state = { clicked: false}
+    state = {clicked: false}
 
     handleClick = () => {
         this.setState({clicked: !this.state.clicked})
@@ -27,7 +28,7 @@ class NavBar extends Component{
     render(){
         return(
             <nav className='NavBarItems'>
-                <h1 className="navbar-logo" > CriptoNoticas <i className='fab fa-react'></i>
+                <h1 className="navbar-logo" > CriptoNoticas <i className='fa-solid fa-bitcoin-sign'></i>
                 </h1>
                 <div className='menu-icon' onClick={this.handleClick}> 
                     <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
@@ -39,14 +40,13 @@ class NavBar extends Component{
                                 <a className={item.cName} href={item.url}>
                                 {item.title}    
                                 </a>
-
                                 </li>
                         )
                     }
 
                     )}
-                    
                 </ul>
+                <Button> Sign up</Button>
             </nav>
         )
     }
